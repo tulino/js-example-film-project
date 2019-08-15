d58c62a32190da604375ce0fa885481b902f5175
tulino <tulin.ozen@bil.omu.edu.tr>
2019-08-16 01:01:45 +0300

    Add items to interface from local storage

--- project.js
+++ project.js
@@ -14,6 +14,10 @@ eventListeners();
 
 function eventListeners(){
     form.addEventListener("submit",addFilm);
+    document.addEventListener("DOMContentLoaded",function(){
+        let films = storage.getFilmsFromStorage();
+        ui.loadAllFilms(films);
+    });
 }
 function addFilm(e){
     const title = titleElement.value;
@@ -30,7 +34,7 @@ function addFilm(e){
         ui.addFilmToUI(newFilm); 
 
         storage.addFilmToStorage(newFilm);
-        
+
         ui.displayMessages("Kayıt başarıyla eklendi.","success")
 
     }
--- ui.js
+++ ui.js
@@ -33,3 +33,21 @@ Ui.prototype.displayMessages = function(message,type){
 		div.remove();
 	},2000);
 }
+
+Ui.prototype.loadAllFilms = function(films){
+	const filmList = document.getElementById("films");
+
+	films.forEach(function(film){
+		filmList.innerHTML += `
+			<tr>
+					<td><img src="${film.url}" class="img-fluid img-thumbnail"></td>
+					<td>${film.title}</td>
+					<td>${film.director}</td>
+					<td><a href="#" id = "delete-film" class = "btn btn-danger">Filmi Sil</a></td>
+			</tr>
+
+		`;
+
+	})
+
+}
