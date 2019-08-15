528dae1b94b354b18dcb031988f63903aa7b6bd4
tulino <tulin.ozen@bil.omu.edu.tr>
2019-08-15 19:39:25 +0300

    Add error and success message

--- project.js
+++ project.js
@@ -17,13 +17,14 @@ function addFilm(e){
     const director = directorElement.value;
     const url = urlElement.value;
 
-    if(title === " " | director === " " | url === " "){
-        // Error msj
+    if(title === "" | director === "" | url === ""){
+        ui.displayMessages("Tüm alanlar doldurulmalıdır...!","danger");
     }
     else{
         // Create new film obj
         const newFilm = new Film(title,director,url);
         ui.addFilmToUI(newFilm); 
+        ui.displayMessages("Kayıt başarıyla eklendi.","success")
 
     }
     ui.clearInputs(titleElement,directorElement,urlElement);
--- ui.js
+++ ui.js
@@ -19,3 +19,17 @@ Ui.prototype.clearInputs = function(element1,element2, element3){
 	element2.value = "";
 	element3.value = "";
 }
+Ui.prototype.displayMessages = function(message,type){
+	const cardBody = document.querySelector(".card-body");
+
+	const div = document.createElement("div");
+
+	div.className = `alert alert-${type}`;
+	div.textContent = message;
+
+	cardBody.appendChild(div);
+
+	setTimeout(function(){
+		div.remove();
+	},2000);
+}
